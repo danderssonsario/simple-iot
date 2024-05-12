1a58e7e5bae35f5820c531f2870c9334bfaa0176
Daniel Andersson <da222xg@student.lnu.se>
2024-05-12 23:01:47 +0200

    Run example code (test commit).

---
+++ boot.py
@@ -0,0 +1,23 @@
+import network
+import socket
+
+ssid = ''
+password = ''
+
+station = network.WLAN(network.STA_IF)
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+    pass
+
+print('Connection')
+print(station.ifconfig())
+
+addr = socket.getaddrinfo('micropython.org', 80)[0][-1]
+s = socket.socket()
+s.connect(addr)
+s.send(b'GET / HTTP/1.1\r\nHost: micropython.org\r\n\r\n')
+data = s.recv(1000)
+print(data)
+s.close()
