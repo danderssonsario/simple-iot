b2b14115399807c0cb136f450fc00459342165ec
Daniel Andersson <da222xg@student.lnu.se>
2024-05-13 11:37:23 +0200

    Subscribe to LED toggle.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import machine
+from machine import Pin
 from time import sleep
 from dht import DHT22
 import ujson
@@ -9,9 +9,10 @@ from umqtt.simple import MQTTClient
 
 # MQTT settings
 MQTT_BROKER = secrets['MQTT_BROKER']
-MQTT_CLIENT_ID = ubinascii.hexlify(machine.unique_id())
+MQTT_CLIENT_ID = secrets['MQTT_CLIENT_ID']
 TEMPERATURE_FEED = secrets['TEMPERATURE_FEED']
 HUMIDITY_FEED = secrets['HUMIDITY_FEED']
+LED_FEED = secrets['LED_FEED']
 PORT = secrets['PORT']
 MQTT_USERNAME = secrets['MQTT_USERNAME']
 MQTT_PASSWORD = secrets['MQTT_PASSWORD']
@@ -20,21 +21,28 @@ MQTT_PASSWORD = secrets['MQTT_PASSWORD']
 DHT_PIN = 15  # Pin GP4 on Raspberry Pi Pico W
 
 # Initialize DHT22 sensor
-dht = DHT22(machine.Pin(DHT_PIN))
+dht = DHT22(Pin(DHT_PIN))
 
+led = Pin("LED", Pin.OUT)
 
 # MQTT callback function
 def mqtt_callback(topic, msg):
     print("Received MQTT message on topic:", topic)
     print("Message:", msg)
-    # Implement your control logic here based on received MQTT messages
-    # For example, control the LED based on received commands
+    if msg == b'1':
+        led.value(1)
+    elif msg == b'0':
+        led.value(0)
 
 # Connect to MQTT broker
 mqtt_client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, PORT, MQTT_USERNAME, MQTT_PASSWORD)
-#mqtt_client.set_callback(mqtt_callback)
+mqtt_client.set_callback(mqtt_callback)
 mqtt_client.connect()
-#mqtt_client.subscribe(b"LED")  # Subscribe to LED control topic
+mqtt_client.subscribe(LED_FEED)
+
+# Sync led with MQTT
+led.value(0)
+mqtt_client.publish(LED_FEED, '1')
 
 # Main loop
 while True:
@@ -53,6 +61,8 @@ while True:
 
       mqtt_client.publish(TEMPERATURE_FEED, ujson.dumps(temperature_data))
       mqtt_client.publish(HUMIDITY_FEED, ujson.dumps(humidity_data))
+      
+      mqtt_client.check_msg()
 
       # Sleep for 60 seconds before next reading
       sleep(3)
