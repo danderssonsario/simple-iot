01251169f9f861e5398e250d5a479de0eefc924d
Daniel Andersson <da222xg@student.lnu.se>
2024-05-23 10:38:01 +0200

    delete files.

--- backend/DTOs/MeasurementDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace backend.DTOs
-{
-    public class MeasurementDTO
-    {
-        public required string Id { get; set; }
-        public required string Value { get; set; }
-        public required string CreatedAt { get; set; }
-    }
-}
